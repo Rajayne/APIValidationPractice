720b8e7f248e77314ee51b9d06458e94ac32de07
Rajayne <ryann.j.nealey@gmail.com>
2023-04-27 16:36:09 -0700

    Create post route to check for valid schema

--- app.js
+++ app.js
@@ -8,12 +8,12 @@ const bookRoutes = require("./routes/books");
 
 app.use("/books", bookRoutes);
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   const err = new ExpressError("Not found!", 404);
   return next(err);
 });
 
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // the default status is 500 Internal Server Error
   let status = err.status || 500;
 
@@ -21,8 +21,8 @@ app.use(function(err, req, res, next) {
   return res.status(status).json({
     error: {
       message: err.message,
-      status: status
-    }
+      status: status,
+    },
   });
 });
 
